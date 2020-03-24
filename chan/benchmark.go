cffd6bf473fc642c7abebebbb7f579cd50b69d0d
rfyiamcool <rfyiamcool@163.com>
2020-03-24 19:19:00 +0800

    update chan

--- chan/benchmark.go
+++ chan/benchmark.go
@@ -13,7 +13,50 @@ func init() {
 
 const count = 10000000
 
-func benchmark(c, buf, sender, receiver int) {
+func dispatchBenchmark(c, buf, sender, receiver int) {
+	start := time.Now()
+
+	balanceNum := 2
+	chpool := make([]chan bool, 0, balanceNum)
+	for i := 0; i < balanceNum; i++ {
+		q := make(chan bool, buf)
+		chpool = append(chpool, q)
+	}
+
+	for index := 0; index < receiver; index++ {
+		idx := index
+		go func() {
+			n := idx % balanceNum
+			for {
+				<-chpool[n]
+			}
+		}()
+	}
+
+	wg := sync.WaitGroup{}
+	step := c / sender
+
+	for i := 0; i < sender; i++ {
+		idx := i
+		wg.Add(1)
+		go func() {
+			n := idx % balanceNum
+			for j := 0; j < step; j++ {
+				chpool[n] <- true
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	cost := time.Since(start)
+	fmt.Printf("dispatch count: %d, chan buf: %d, sender: %d, recevier: %d, time cost: %s, qps: %.2f \n",
+		c, buf, sender, receiver, time.Since(start), float64(count)/cost.Seconds(),
+	)
+}
+
+func directBenchmark(c, buf, sender, receiver int) {
 	start := time.Now()
 	ch := make(chan bool, buf)
 
@@ -41,34 +84,68 @@ func benchmark(c, buf, sender, receiver int) {
 	wg.Wait()
 	close(ch)
 
-	fmt.Printf("count: %d, chan buf: %d, sender: %d, recevier: %d, time cost: %s \n",
-		c, buf, sender, receiver, time.Now().Sub(start),
+	cost := time.Since(start)
+	fmt.Printf("direct  count: %d, chan buf: %d, sender: %d, recevier: %d, time cost: %s, qps: %.2f \n",
+		c, buf, sender, receiver, time.Since(start), float64(count)/cost.Seconds(),
 	)
 }
 
 func main() {
-	benchmark(count, 0, 10, 10)
-	benchmark(count, 100, 10, 10)
+	directBenchmark(count, 0, 10, 10)
+	dispatchBenchmark(count, 0, 10, 10)
+
+	directBenchmark(count, 50, 10, 10)
+	dispatchBenchmark(count, 50, 10, 10)
+
+	directBenchmark(count, 100, 10, 10)
+	dispatchBenchmark(count, 100, 10, 10)
+
+	directBenchmark(count, 1000, 10, 10)
+	dispatchBenchmark(count, 1000, 10, 10)
 	time.Sleep(1 * time.Second)
 	fmt.Println()
 
-	benchmark(count, 0, 50, 50)
-	benchmark(count, 100, 50, 50)
+	directBenchmark(count, 0, 50, 50)
+	dispatchBenchmark(count, 0, 50, 50)
+	directBenchmark(count, 100, 50, 50)
+	dispatchBenchmark(count, 100, 50, 50)
+	directBenchmark(count, 1000, 50, 50)
+	dispatchBenchmark(count, 1000, 50, 50)
 	time.Sleep(1 * time.Second)
 	fmt.Println()
 
-	benchmark(count, 0, 100, 100)
-	benchmark(count, 100, 100, 100)
+	directBenchmark(count, 0, 100, 100)
+	dispatchBenchmark(count, 0, 100, 100)
+
+	directBenchmark(count, 100, 100, 100)
+	dispatchBenchmark(count, 100, 100, 100)
+
+	directBenchmark(count, 1000, 100, 100)
+	dispatchBenchmark(count, 1000, 100, 100)
+
 	time.Sleep(1 * time.Second)
 	fmt.Println()
 
-	benchmark(count, 0, 200, 200)
-	benchmark(count, 100, 200, 200)
+	directBenchmark(count, 0, 200, 200)
+	dispatchBenchmark(count, 0, 200, 200)
+
+	directBenchmark(count, 100, 200, 200)
+	dispatchBenchmark(count, 100, 200, 200)
+
+	directBenchmark(count, 1000, 200, 200)
+	dispatchBenchmark(count, 1000, 200, 200)
 	time.Sleep(1 * time.Second)
 	fmt.Println()
 
-	benchmark(count, 0, 300, 300)
-	benchmark(count, 100, 300, 300)
+	directBenchmark(count, 0, 300, 300)
+	dispatchBenchmark(count, 0, 300, 300)
+
+	directBenchmark(count, 100, 300, 300)
+	dispatchBenchmark(count, 100, 300, 300)
+
+	directBenchmark(count, 1000, 300, 300)
+	dispatchBenchmark(count, 1000, 300, 300)
+
 	time.Sleep(1 * time.Second)
 	fmt.Println()
 }
